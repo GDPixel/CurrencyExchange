df14c817ead801499e9efc4be8e2d59f4486348b
gdpixel <kulikov87dev@gmail.com>
2026-09-12 01:29:41 +0300

    add ExchangeRate model

---
+++ src/main/java/model/ExchangeRate.java
@@ -0,0 +1,58 @@
+package model;
+
+public class ExchangeRate {
+    private int id;
+    private int baseCurrencyId;
+    private int targetCurrencyId;
+    //TODO rate должен сохранять точность double не подходит
+    private double rate;
+
+    public ExchangeRate(int id, int baseCurrencyId, int targetCurrencyId, double rate) {
+        this.id = id;
+        this.baseCurrencyId = baseCurrencyId;
+        this.targetCurrencyId = targetCurrencyId;
+        this.rate = rate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBaseCurrencyId() {
+        return baseCurrencyId;
+    }
+
+    public void setBaseCurrencyId(int baseCurrencyId) {
+        this.baseCurrencyId = baseCurrencyId;
+    }
+
+    public int getTargetCurrencyId() {
+        return targetCurrencyId;
+    }
+
+    public void setTargetCurrencyId(int targetCurrencyId) {
+        this.targetCurrencyId = targetCurrencyId;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeRate{" +
+               "id=" + id +
+               ", baseCurrencyId=" + baseCurrencyId +
+               ", targetCurrencyId=" + targetCurrencyId +
+               ", rate=" + rate +
+               '}';
+    }
+}
